7ed491e59969238409c8a253d14f3d32ab85dbc8
Coeurxx <2543046796@qq.com>
2024-06-02 23:26:25 +0800

    chore: 删除无用

--- typed-router.d.ts
+++ typed-router.d.ts
@@ -20,6 +20,5 @@ declare module 'vue-router/auto-routes' {
   export interface RouteNamedMap {
     '/': RouteRecordInfo<'/', '/', Record<never, never>, Record<never, never>>,
     '/[...all]': RouteRecordInfo<'/[...all]', '/:all(.*)', { all: ParamValue<true> }, { all: ParamValue<false> }>,
-    '/home/[name]': RouteRecordInfo<'/home/[name]', '/home/:name', { name: ParamValue<true> }, { name: ParamValue<false> }>,
   }
 }
